2d2832dedafcae2580c756feca6d377de48724ee
JACKOWSKI Pawel <pawel.jackowski-ext@leica-geosystems.com>
2023-03-19 12:11:57 +0100

    feat: add interaction test

--- .storybook/main.js
+++ .storybook/main.js
@@ -10,7 +10,11 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "storybook-addon-designs",
+    "@storybook/addon-interactions",
   ],
+  features: {
+    interactionsDebugger: true, // 👈 Enable playback controls
+  },
   core: {
     builder: {
       name: "webpack5",
--- src/InboxScreen.stories.js
+++ src/InboxScreen.stories.js
@@ -3,6 +3,9 @@ import { rest } from "msw";
 import { InboxScreen } from "./InboxScreen";
 import { Default as TaskListDefault } from "./components/TaskList.stories";
 
+import { within, userEvent, findByRole } from "@storybook/testing-library";
+import { expect } from "@storybook/jest";
+
 export default {
   component: InboxScreen,
   title: "InboxScreen",
@@ -19,8 +22,6 @@ Default.parameters = {
       }),
     ],
   },
-};
-Default.parameters = {
   design: {
     type: "figma",
     url: "https://www.figma.com/file/hoYgD8YUzvigjwDhWveBbf/TestStorybook?node-id=4-17&t=gjkACJvUMEfNYpAR-0",
@@ -39,10 +40,28 @@ Error.parameters = {
       }),
     ],
   },
-};
-Error.parameters = {
   design: {
     type: "figma",
     url: "https://www.figma.com/file/hoYgD8YUzvigjwDhWveBbf/TestStorybook?node-id=4-29&t=gjkACJvUMEfNYpAR-0",
   },
+};
+
+export const PinTask = Template.bind({});
+PinTask.parameters = Default.parameters;
+PinTask.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const getTask = (name) => canvas.findByRole("listitem", { name });
+
+  // Find the task to pin
+  const itemToPin = await getTask("Export logo");
+
+  // Find the pin button
+  const pinButton = await findByRole(itemToPin, "button", { name: "pin" });
+
+  // Click the pin button
+  await userEvent.click(pinButton);
+
+  // Check that the pin button is now a unpin button
+  const unpinButton = within(itemToPin).getByRole("button", { name: "unpin" });
+  await expect(unpinButton).toBeInTheDocument();
 };
